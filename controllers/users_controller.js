ba2687c9352f75560563baef3d1be966a68cf546
Deepesh Sharma <deep92112018@gmail.com>
2023-04-21 19:52:47 +0530

    setting up user schema

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -5,6 +5,18 @@ module.exports.profile = function(req, res) {
     });
 }
 
+// render the sign up page
+module.exports.signIn = function(req, res) {
+    return res.render('user_sign_in', {
+        title: 'Codeial | Sign In'
+    })
+}
 
+// render the sign in page
+module.exports.signUp = function(req, res) {
+    return res.render('user_sign_up', {
+        title: 'Codeial | Sign Up'
+    })
+}
 
 
