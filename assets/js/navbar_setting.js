062b1aa6bfc9ede1f26d37473f0e443938522732
Deepesh Sharma <deep92112018@gmail.com>
2023-05-03 16:30:49 +0530

    add scss to header, footer, profile, signin and signup

---
+++ assets/js/navbar_setting.js
@@ -0,0 +1,6 @@
+var settingsMenu = document.querySelector('.settings-menu');
+console.log(settingsMenu);
+
+function settingsMenuToggle() {
+    settingsMenu.classList.toggle('settings-menu-height');
+}
