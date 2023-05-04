aaa34e39daae52cea82c4c575b8e7350b93763c7
Deepesh Sharma <deep92112018@gmail.com>
2023-05-04 17:28:36 +0530

    Updating : Saving Posts to the DB

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,16 +1,29 @@
 const Post = require('../models/post')
 
 // creating Post
-module.exports.create = function(req, res) {
+module.exports.create = function(req, res){
+    if (!req.user || !req.user._id) {
+        console.log('Error: User is not authenticated or does not have an _id');
+        return res.redirect('back');
+    }
+
+    if (!req.body || !req.body.content) {
+        console.log('Error: Request body is missing content');
+        return res.redirect('back');
+    }
+
+    console.log('Creating a new post with content:', req.body.content);
+
     Post.create({
         content: req.body.content,
-        user: req.user._id,
-    }, function(err, newPost){
-        if(err) {
-            console.log('Error in creating a post', err);
-            return;
+        user: req.user._id
+    }, function(err, post){
+        if(err){
+            console.log('Error in creating a post:', err);
+            return res.redirect('back');
         }
-        console.log('*********', newPost);
+
+        console.log('Post created successfully:', post);
         return res.redirect('back');
     });
-}
+};
