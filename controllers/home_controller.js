4988463aca385929023f6831d2c422b16fcb4646
Deepesh Sharma <deep92112018@gmail.com>
2023-05-04 17:33:11 +0530

    Display Posts and related User

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,11 +1,24 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res) {
     console.log(req.cookies);
     // res.cookie('user_id', 25);
-    return res.render('home', {
-        title: 'Home'
-    });
+    // Post.find({}, function(err, post) {
+    //     if(err) {
+    //         console.log('error in fetching post from db');
+    //         return;
+    //     }
+    //     return res.render('home', {
+    //         title: 'Home',
+    //         post_list: post
+    //     });
+    // });
+
+    // populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: 'Home',
+            post_list: posts
+        });
+    })
 }
-
-// module.exports.actionName = function(req, res) {}
-
-
