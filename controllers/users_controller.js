04a55e0675011dd506035e209e5b2d55198bcfa5
Deepesh Sharma <deep92112018@gmail.com>
2023-05-27 15:16:41 +0530

    Adding Flash Messages to signup page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -47,6 +47,7 @@ module.exports.signIn = function(req, res) {
 // get the sign up data
 module.exports.create = function(req, res) {
     if (req.body.password != req.body.confirm_password) {
+        req.flash('error', 'Passwords do not match');
         return res.redirect('back');
     }
 
@@ -60,6 +61,7 @@ module.exports.create = function(req, res) {
                 return res.redirect('/users/sign-in');
             })
         } else {
+            req.flash('success', 'You have signed up, login to continue!');
             return res.redirect('back');
         }
     })
