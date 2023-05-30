555ffb1ecfdd0ffd3b501c39c7ba6c00799c047e
Deepesh Sharma <deep92112018@gmail.com>
2023-05-30 23:06:38 +0530

    Multer - Showing the Avatar in user profile, Connecting the Dots

--- index.js
+++ index.js
@@ -33,6 +33,9 @@ app.use(express.static('./assets'));
 // use express layouts
 app.use(expressLayouts);
 
+// make the uploads path available to the browser
+app.use('/uploads', express.static(__dirname + '/uploads'));
+
 // extract style and scripts from sub pages into the layout
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
