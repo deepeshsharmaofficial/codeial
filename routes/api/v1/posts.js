9d0f13f8f917166810ec747f5aa5a5f634fdcb7c
Deepesh Sharma <deep92112018@gmail.com>
2023-06-10 00:31:08 +0530

    Authentication and Authorization

--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -1,10 +1,20 @@
 const express = require('express');
 
 const router = express.Router();
+const passport = require('passport');
 const postsApi = require("../../../controllers/api/v1/posts_api");
 
 router.get('/', postsApi.index);
-router.delete('/:id', postsApi.destroy);
 
+// I am setting the session to be false because I don't want session cookies to be generated
+// This will put an authentication check over my passport.
+router.delete('/:id', passport.authenticate('jwt', {session: false}) ,postsApi.destroy);
+
+
+/* I also need to check if this authentication is working for this We will need a couple of things.
+
+1. The authentication token jwt for the user, once that user session has been created.
+2. Post which needs to be deleted
+*/
 
 module.exports = router;
