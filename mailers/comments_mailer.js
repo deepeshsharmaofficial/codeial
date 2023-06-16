03b6605dd0849869de82ee5523e7b786d4a6d00d
Deepesh Sharma <deep92112018@gmail.com>
2023-06-16 23:28:58 +0530

    Send HTML Template Emails

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -4,11 +4,13 @@ const nodeMailer = require('../config/nodemailer');
 exports.newComment = (comment) => {
     console.log('inside new comment mailer', comment);
 
+    let htmlString = nodeMailer.renderTemplate({comment: comment}, '/comments/new_comment.ejs');
+
     nodeMailer.transporter.sendMail({
         from: 'deep921155@gmail.com',
         to: comment.user.email,
         subject: "New Comment Published",
-        html: '<h1> Yup, your comment is published</h1>'
+        html: htmlString
     }, (err, info) => {
         if(err) {
             console.log('Error in sending mail', err);
